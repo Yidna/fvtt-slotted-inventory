b277dc407abd8e4dc554356305f48d3b3dd24cbc
Andy Siu <andy@typecaststudios.com>
2021-04-21 16:49:34 -0700

    slimmed down inventory template
    - adjusted sheet data to render new inventory format

--- src/slotted-inventory-sheet.js
+++ src/slotted-inventory-sheet.js
@@ -5,4 +5,21 @@ export class SlottedInventorySheet extends ActorSheet5eCharacter {
         if ( !game.user.isGM && this.actor.limited ) return 'systems/dnd5e/templates/actors/limited-sheet.html';
         return 'modules/fvtt-slotted-inventory/src/templates/character-sheet.html';
     }
+
+    getData() {
+        const slottedInventory = this.actor.getFlag('fvtt-slotted-inventory', 'inventory');
+        return {
+            ...super.getData(),
+            inventory: Object.keys(slottedInventory).reduce((inventory, key) => {
+                inventory[key] = {
+                    ...slottedInventory[key],
+                    slots: slottedInventory[key].slots.map(slot => ({
+                        ...slot,
+                        item: this.actor.items.get(slot.item)?.data
+                    }))
+                };
+                return inventory;
+            }, {})
+        }
+    }
 }
