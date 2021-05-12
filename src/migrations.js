164a9b17db6d6b7ba2122a73d43041afe4b3149d
Andy <mrclock9090@yahoo.com>
2021-05-12 03:29:12 -0700

    bugfix-1-2 -- migration and sanitization bugs (#3)
    - resolves #1
    - resolves #2

--- src/migrations.js
+++ src/migrations.js
@@ -1,14 +1,19 @@
 import { v0_0_0 } from './migrations/v0_0_0__initialize-inventory-system.js';
+import { v0_0_1 } from './migrations/v0_0_1__filter-invalid-item-types.js';
 import { FlagManager } from './flag-manager.js';
 
 export async function migrate(player) {
     const migrationFns = [{
         v: undefined,
         fn: v0_0_0
+    }, {
+        v: '0.0.1',
+        fn: v0_0_1
     }];
     return migrationFns.reduce(async (player, migration) => {
-        if (FlagManager.getVersion(player) === migration.v) {
-            return await migration.fn(player);
+        const actor = await player;
+        if (FlagManager.getVersion(actor) === migration.v) {
+            return await migration.fn(actor);
         }
         return player;
     }, player);
